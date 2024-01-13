71e0850481e2692f51e187c5eb3cc255c31ccb9a
dmgrabski <dmgrabski97@gmail.com>
2024-01-13 11:38:08 +0100

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,27 @@
+import Profile from './profile/Profile.js';
+import user from './User.json';
+import Statistics from './statistics/Statistics.js';
+import data from './data.json';
+import FriendList from './friendList/FriendList.js';
+import friends from './friends.json';
+import TransactionHistory from './transactionHistory/TransactionHistory.js';
+import transactions from './transactions.json';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template .
-    </div>
+    <>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="upload stats" stats={data} />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
+    </>
   );
 };
+
+export default App;
